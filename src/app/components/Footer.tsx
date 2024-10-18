3b46d0c22e65db0b752830cbaeafdf38d6cd70ca
Devpe007 <anddev007pe@gmail.com>
2024-10-18 15:25:55 -0400

    update footer icons positions

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,23 +8,21 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <div id='contact-us' className="flex flex-col items-center justify-center gap-4 bg-gray-900">
-      <section className="flex gap-4 mt-4">
-        <div>
+      <section className="flex mt-4 items-center justify-between w-[300px] lg:w-[1000px]">
+        <div className="flex items-center">
+          <Image src={logoElo} alt="logo" width={56} />
+          <h4 className="text-white">ELO DIGITAL</h4>
+        </div>
+
+        <div className="flex gap-4">
           <Link href='https://www.instagram.com/agenciaelo.digital/'>
             <Image src={instaLogo} alt="logo instagram" width={26} />
           </Link>
-        </div>
 
-        <div>
           <Link href='https://www.facebook.com/elodigitalagencia'>
-            <Image src={facebookLogo} alt="logo facebook" width={26} />
+            <Image src={facebookLogo} alt="logo facebook" width={27} />
           </Link>
         </div>
-      </section>
-
-      <section className="flex items-center">
-        <Image src={logoElo} alt="logo" width={56} />
-        <h4 className="text-white">ELO DIGITAL</h4>
       </section>
 
       <section className="mb-4">
