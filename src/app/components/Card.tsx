72d61431da0ab243e2d09a15b83117603478bb2a
Devpe007 <anddev007pe@gmail.com>
2024-10-18 16:40:13 -0400

    highlighting the key words of the slide

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -7,7 +7,7 @@ type CardProps = {
 
 export default function Card({ children, title }: CardProps) {
   return (
-    <div className="flex flex-col items-center w-80 h-[380px] bg-white px-8 py-14 lg:px-10 rounded">
+    <div className="flex flex-col items-center w-80 h-[380px] bg-white px-8 py-14 lg:px-10 rounded shadow-xl">
       <h1 className="text-3xl mb-8">{title}</h1>
 
       <p className="text-lg">
--- src/app/components/Slider.tsx
+++ src/app/components/Slider.tsx
@@ -9,13 +9,6 @@ import 'swiper/css/pagination'
 import Card from './Card';
 
 export default function Slider() {
-  const data = [
-    { id: 1, title: 'Programação Personalizada', description: 'Criamos soluções sob medida que otimizam seus processos e melhoram a experiência do cliente.' },
-    { id: 2, title: 'Automação de Atendimento', description: 'Implementamos sistemas que garantem um atendimento rápido e eficiente, 24/7.' },
-    { id: 3, title: 'Automação de Processos Internos', description: 'Simplificamos e aceleramos seus fluxos de trabalho, permitindo que sua equipe foque no que realmente importa.' },
-    { id: 4, title: 'Integração com IA', description: 'Usamos inteligência artificial para criar atendimentos inteligentes e personalizados, otimizando a conversão e a retenção de clientes.' },
-  ]
-
   return (
     <div className='flex justify-center items-center ml-[36px] lg:w-[1000px]'>
       <Swiper
@@ -37,14 +30,30 @@ export default function Slider() {
         loop
         centeredSlides
       >
-        {data.map((item) => (
-          <SwiperSlide key={item.id} className='w-[40px]'>
-            <Card title={item.title}>
-              {item.description}
-            </Card>
-          </SwiperSlide>
-        ))}
+        <SwiperSlide className='w-[40px]'>
+          <Card title="Programação Personalizada">
+            Criamos <span className='text-primary-orange'>soluções</span> sob medida que <span className='text-primary-orange'>otimizam</span> seus processos e melhoram a <span className='text-primary-orange'>experiência</span> do cliente.
+          </Card>
+        </SwiperSlide>
+
+        <SwiperSlide className='w-[40px]'>
+          <Card title="Automação de Atendimento">
+            Implementamos <span className='text-primary-orange'>sistemas</span> que garantem um <span className='text-primary-orange'>atendimento rápido e eficiente, 24/7</span>.
+          </Card>
+        </SwiperSlide>
+
+        <SwiperSlide className='w-[40px]'>
+          <Card title="Automação de Processos Internos">
+            Simplificamos e aceleramos seus <span className='text-primary-orange'>fluxos de trabalho</span>, permitindo que sua equipe foque no que realmente importa.
+          </Card>
+        </SwiperSlide>
+
+        <SwiperSlide className='w-[40px]'>
+          <Card title="Integração com IA">
+            Usamos <span className='text-primary-orange'>inteligência artificial</span> para criar <span className='text-primary-orange'>atendimentos inteligentes e personalizados</span>, otimizando a conversão e a retenção de <span className='text-primary-orange'>clientes</span>.
+          </Card>
+        </SwiperSlide>
       </Swiper>
-    </div>
+    </div >
   )
 }
